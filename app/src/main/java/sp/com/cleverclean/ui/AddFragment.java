5bd0f2d85a4352630a9cb01673d41afefee423ef
SaltyHaise <josephloh03@gmail.com>
2022-02-15 23:04:05 +0800

    image button works

--- app/src/main/java/sp/com/cleverclean/ui/AddFragment.java
+++ app/src/main/java/sp/com/cleverclean/ui/AddFragment.java
@@ -1,5 +1,7 @@
 package sp.com.cleverclean.ui;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.DialogInterface;
@@ -14,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,18 +28,20 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import java.io.IOException;
+
 import sp.com.cleverclean.GPSTracker;
 import sp.com.cleverclean.MyDatabaseHelper;
 import sp.com.cleverclean.R;
 
 
 
-public class AddFragment extends Fragment {
+public class AddFragment extends Fragment implements View.OnClickListener{
     private EditText BinName;
     private EditText BinAddress;
-    private Button BinImage;
+    private Button ImageButton;
     private Button buttonSave;
-
+    private ImageView BinImage;
     private TextView location;
     private double latitude = 0.0d;
     private double longitude = 0.0d;
@@ -47,7 +52,7 @@ public class AddFragment extends Fragment {
     private Bitmap bitmap = null;
     private Uri imageUri;
 
-    private String name, addr, timeStamp;
+
 
     @Nullable
     @Override
@@ -57,12 +62,48 @@ public class AddFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_addbin, container, false);
         BinName = view.findViewById(R.id.BinNo);
         BinAddress = view.findViewById(R.id.BinLocation);
-        BinImage = view.findViewById(R.id.imagebutton);
+        BinImage = view.findViewById(R.id.BinImage);
+        ImageButton = view.findViewById(R.id.imagebutton);
+        ImageButton.setOnClickListener(this);
         buttonSave = view.findViewById(R.id.save);
+        buttonSave.setOnClickListener(this);
         location = view.findViewById(R.id.location);
         getLocation = view.findViewById(R.id.getLocation);
+        getLocation.setOnClickListener(this);
         return view;
 
 
     }
-}
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imagebutton:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+                break;
+
+        }
+    }
+
+        public int PICK_IMAGE = 1; //request code:
+        @Override
+        public void onActivityResult ( int requestCode, int resultCode, Intent data){
+            if (resultCode == RESULT_OK) {
+                if (requestCode == PICK_IMAGE) {
+                    imageUri = data.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (imageUri != null) {
+                        BinImage.setImageURI(imageUri);
+                    }
+                }
+            }
+        }
+
+    }
